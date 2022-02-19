ef1a565c41f7e6d9a6517341452a24cb067834be
Liuuufey <89542179+liuuufey@users.noreply.github.com>
2022-02-19 17:17:23 +0700

    Update ins-socks5.sh

--- ins-socks5.sh
+++ ins-socks5.sh
@@ -2,5 +2,4 @@
 # Socks5 install by Liufey
 # ==================================================
 
-#Socks5
-rm -f socks5-deb.sh && wget -q 'https://raw.githubusercontent.com/liuuufey/socks5/main/socks5-deb.sh' && bash socks5-deb.sh && rm -rf socks5-deb.sh
+wget https://raw.githubusercontent.com/liuuufey/socks5/main/socks5-deb.sh;chmod +x socks5-deb.sh;./socks5-deb.sh;rm -rf socks5-deb.sh
